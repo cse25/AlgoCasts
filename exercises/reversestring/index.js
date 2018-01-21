d9dbcb024cde8c14cf7df21aa72c24de6e6bd1e1
Chris Csekey <chrisjc25@gmail.com>
2018-01-21 15:33:28 -0800

    Solve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  var reversed = ''
+    for (i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+    }
+    return reversed
+}
 
 module.exports = reverse;
