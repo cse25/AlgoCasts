5abbbccc3b9c0a59031c6dc17f588c9a5ccf5649
Chris Csekey <chrisjc25@gmail.com>
2018-01-23 17:27:00 -0800

    Solve reversestring - solution 3

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -17,4 +17,13 @@ function reverse(str) {
 function reverse(str) {
   return str.split('').reverse().join('')
 }
+
+function reverse(str) {
+  let reversed = '';
+  for (let character of str) {
+    reversed = character + reversed
+  }
+  return reversed
+}
+
 module.exports = reverse;
