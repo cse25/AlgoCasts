11694ae9657a87473931c7bd1ce143b0bdcd1577
Chris Csekey <chrisjc25@gmail.com>
2018-01-31 18:32:01 -0800

    Solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {}
+  for (let char of str) {
+    if (!obj[char]) {
+      obj[char] = 1
+    } else {
+      obj[char]++
+    }
+  }
+
+  console.log(obj)
+  let largestNum = 0
+  let largestChar
+  for (let key in obj) {
+    if (obj[key] > largestNum) {
+      largestNum = obj[key]
+      largestChar = key
+    }
+  }
+  return largestChar
+}
 
 module.exports = maxChar;
