9339b1455de79f0a0f1453239055bec1e06b3e70
Chris Csekey <chrisjc25@gmail.com>
2018-02-05 20:00:06 -0800

    Solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = []
+
+  while (array.length !== 0) {
+    chunked.push(array.splice(0, size))
+  }
+
+  return chunked
+}
+
 
 module.exports = chunk;
